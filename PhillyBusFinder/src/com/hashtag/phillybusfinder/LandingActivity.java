3a3723d1b751d45d055b38d8f237e27fea312620
Michael Henry <mike@mhenry.io>
2014-02-22 16:45:56 -0500

    Google Maps!!

--- PhillyBusFinder/src/com/hashtag/phillybusfinder/LandingActivity.java
+++ PhillyBusFinder/src/com/hashtag/phillybusfinder/LandingActivity.java
@@ -1,23 +1,27 @@
 package com.hashtag.phillybusfinder;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class LandingActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+public class LandingActivity extends SherlockActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // set the content view
         setContentView(R.layout.activity_landing);
+        // configure the SlidingMenu
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+//        menu.setMenu(R.layout.menu);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.landing, menu);
-        return true;
-    }
-    
 }
