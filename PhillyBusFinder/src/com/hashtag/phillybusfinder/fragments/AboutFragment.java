cd0487ef5a48104999590dc5fbe02b8b9b3d7172
Michael Henry <mike@mhenry.io>
2014-02-23 11:39:03 -0500

    Removing custom typeface.

--- PhillyBusFinder/src/com/hashtag/phillybusfinder/fragments/AboutFragment.java
+++ PhillyBusFinder/src/com/hashtag/phillybusfinder/fragments/AboutFragment.java
@@ -1,6 +1,5 @@
 package com.hashtag.phillybusfinder.fragments;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.hashtag.phillybusfinder.R;
-import com.hashtag.phillybusfinder.ui.Util;
 
 public class AboutFragment extends Fragment {
 
@@ -18,8 +16,6 @@ public class AboutFragment extends Fragment {
         View view = inflater.inflate(R.layout.about, container, false);
         TextView textView = (TextView) view.findViewById(R.id.textView);
         textView.setText(getActivity().getResources().getString(R.string.about));
-        Typeface mFontFace = Util.getTypeface(getActivity(), "fonts/Lato.ttf");
-        textView.setTypeface(mFontFace);
         return view;
     }
 
