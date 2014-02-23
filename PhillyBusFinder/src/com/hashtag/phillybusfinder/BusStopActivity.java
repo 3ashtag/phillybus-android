41d5753ff6d533310f3392c8fc3f7504d8d9d965
Michael Henry <mike@mhenry.io>
2014-02-23 04:22:55 -0500

    Adding BusStopActivity

---
+++ PhillyBusFinder/src/com/hashtag/phillybusfinder/BusStopActivity.java
@@ -0,0 +1,15 @@
+package com.hashtag.phillybusfinder;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class BusStopActivity extends SherlockActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_stop);
+    }
+
+}
